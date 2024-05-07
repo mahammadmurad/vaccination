7947cc9c2869fa1286952db4209298f636fa3bb7
mahammadmurad <mahammad.muradd@gmail.com>
2024-05-08 01:01:17 +0400

    models created for all apps

--- vaccination/admin.py
+++ vaccination/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Vaccination
+
+admin.site.register(Vaccination)
 
 # Register your models here.
--- vaccine/admin.py
+++ vaccine/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from vaccine.models import Vaccine
 
+admin.site.register(Vaccine)
 # Register your models here.
