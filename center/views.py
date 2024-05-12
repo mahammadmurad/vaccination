af7b15e544cfc67497b4154f5efaad932d9874e4
mahammadmurad <mahammad.muradd@gmail.com>
2024-05-13 00:26:34 +0400

    function based views created

--- center/views.py
+++ center/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
+from .models import Center
+from center.forms import CenterForm
+from django.http import HttpResponseRedirect, Http404
+from django.urls import reverse
+from django.contrib import messages
 
-# Create your views here.
+
+def center_list(request):
+    centers = Center.objects.all()
+    context = {
+        'centers': centers
+    }
+    return render(request, 'center/center_list.html', context)
+
+
+def center_detail(request, pk):
+    center = Center.objects.get(pk=pk)
+    context = {"center": center}
+    return render(request, "center/center_detail.html", context)
+
+
+def create_center(request):
+
+    if request.method == 'POST':
+        form = CenterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('center:list'))
+        return render(request, "center/create_center.html", {'form': form})
+
+    context = {
+        'form': CenterForm()
+    }
+
+    return render(request, 'center/create_center.html', context)
+
+
+def update_center(request, pk):
+    try:
+        center = Center.objects.get(pk=pk)
+    except Center.DoesNotExist:
+        raise Http404("Center does not exist")
+
+    if request.method == 'POST':
+        form = CenterForm(request.POST, instance=center)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("center:detail", kwargs={'pk': center.pk}))
+        return render(request, "center/update_center.html", {'form': form})
+    context = {"form": CenterForm(instance=center)}
+
+    return render(request, 'center/update_center.html', context)
+
+
+def delete_center(request, pk):
+    try:
+        center = Center.objects.get(pk=pk)
+    except Center.DoesNotExist:
+        raise Http404("Center does not exist")
+
+    if request.method == "POST":
+        center.delete()
+        messages.success(request, "Vaccination Center Deleted Successfully")
+        return HttpResponseRedirect(reverse("center:list"))
+
+    context = {
+        "center": center,
+    }
+    return render(request, "center/delete_center.html", context)
--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,7 +23,13 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index, name='index'),
+    path('center/', include('center.urls', namespace='center')),
+    # path('vaccine/', include('vaccine.urls')),
+    # path('campaign/', include('campaign.urls')),
+    # path('vaccination/', include('vaccination.urls')),
+    # path('user/', include('user.urls')),
+    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
